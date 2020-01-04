db523dc60d9392f495bb23be4548b206a37f4760
LisaCee <lisaceedesign@gmail.com>
2020-01-03 17:45:33 -0800

    singly linked list from lecture

---
+++ linked_list.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print(self):
+        curr_node = self.head
+        print('---HEAD---')
+        while curr_node is not None:
+            print(curr_node.value)
+            curr_node = curr_node.next
+        print('------')
+
+    def add_to_front(self, value):
+        old_head = self.head
+        new_head = Node(value)
+        new_head.next = old_head
+        self.head = new_head
+
+
+ll = LinkedList()
+ll.add_to_front(7)
+ll.add_to_front(3)
+ll.add_to_front(1)
+ll.print()
